749ce6f1b3d9529c119c44cce63d65e08e3f18a3
Juaun2002 <jvpessoa113@gmail.com>
2025-09-10 10:41:57 -0300

    segundo commmit

---
+++ src/app/contato/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+const Contato = () => {
+  return (
+    <div><h1>contatos</h1></div>
+  )
+}
+
+export default Contato
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,40 @@
+"use client";
+
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse, faCopy, faPhone } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center gap-4 py-4 bg-gray-400">
-        <h1 className="text-3xl font-bold hover:text-emerald-600 transition-colors duration-300 transform hover:scale-110 transform-gpu ">Gerador de senhas</h1>
+        <h1 className="text-3xl font-bold hover:text-emerald-600 transition-colors duration-300 transform hover:scale-110 transform-gpu ">
+          Gerador de senhas
+        </h1>
         <ul className="flex flex-wrap items-center justify-center gap-2 text-sm font-medium ">
-
           <li className="hover:text-emerald-600">
-            <a href="#"> <FontAwesomeIcon icon={faHouse} className="w-4 h-4 text-2xl" /></a>
+            <Link href="./">
+              <FontAwesomeIcon icon={faHouse} className="w-4 h-4 text-2xl" />
+            </Link>
           </li>
 
           <li className="hover:text-emerald-600">
-            <a href="#"><FontAwesomeIcon icon={faCopy} className="w-4 h-4 text-2xl" /></a>
+            <Link href="./contato">
+              <FontAwesomeIcon icon={faCopy} className="w-4 h-4 text-2xl" />
+            </Link>
           </li>
 
           <li className="hover:text-emerald-600">
-            <a href="#">
-              <FontAwesomeIcon icon={faPhone}  className="w-4  h-4 text-2xl"  />
+            <a
+              href="https://wa.me/5583988639653"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faPhone} className="w-4 h-4 text-2xl" />
             </a>
           </li>
-
         </ul>
-
       </div>
     </>
   );
