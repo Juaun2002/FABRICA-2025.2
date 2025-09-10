4898eb646e9147488b7ab344f2a9d555db0fb137
Juaun2002 <jvpessoa113@gmail.com>
2025-09-10 01:03:41 -0300

    aa

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faCopy, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 
 
@@ -9,20 +9,20 @@ const Header = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center gap-4 py-4 bg-gray-400">
-        <h1 className="text-3xl font-bold">Gerador de senhas</h1>
+        <h1 className="text-3xl font-bold hover:text-emerald-600 transition-colors duration-300 transform hover:scale-110 transform-gpu ">Gerador de senhas</h1>
         <ul className="flex flex-wrap items-center justify-center gap-2 text-sm font-medium ">
 
           <li className="hover:text-emerald-600">
-            <a href="#"> <FontAwesomeIcon icon={faHouse} className="w-4 h-4" /></a>
+            <a href="#"> <FontAwesomeIcon icon={faHouse} className="w-4 h-4 text-2xl" /></a>
           </li>
 
           <li className="hover:text-emerald-600">
-            <a href="#">Sobre</a>
+            <a href="#"><FontAwesomeIcon icon={faCopy} className="w-4 h-4 text-2xl" /></a>
           </li>
 
           <li className="hover:text-emerald-600">
-            <a href="#" className="facebook">
-              Contato
+            <a href="#">
+              <FontAwesomeIcon icon={faPhone}  className="w-4  h-4 text-2xl"  />
             </a>
           </li>
 
